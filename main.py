affe177074b51b5ec551cc6026e7518d788e45bf
Euryn <crossdeltavi@gmail.com>
2023-09-17 19:07:22 +0200

    renamed elte practice directory

---
+++ main.py
@@ -0,0 +1,14 @@
+def main() -> None:
+    def expression(A: bool, B: bool) -> bool:
+        # Absorption law -- left absorption form
+        return (A or (A and B)) == A
+
+    FIELD: list[bool] = [True, False]
+
+    print(
+        all((A or (A and B)) == A for A in FIELD for B in FIELD)
+    )
+
+
+if __name__ == "__main__":
+    main()
