40fc90707f13c856d3e446956868f11144dd2f66
Euryn <crossdeltavi@gmail.com>
2023-09-19 22:10:38 +0200

    added useful resourecs

--- README.md
+++ README.md
@@ -4,4 +4,6 @@ A collection of Python exercises.
 
 ## Resources
 
--
+- **Official documentation**: https://docs.python.org/3/
+- **PEPs**: https://peps.python.org/
+- **General style conventions**: https://peps.python.org/pep-0008/
--- main.py
+++ main.py
@@ -1,13 +1,15 @@
+
 def main() -> None:
-    def expression(A: bool, B: bool) -> bool:
-        # Absorption law -- left absorption form
-        return (A or (A and B)) == A
+    pass
+    # def expression(A: bool, B: bool) -> bool:
+    #     # Absorption law -- left absorption form
+    #     return (A or (A and B)) == A
 
-    FIELD: list[bool] = [True, False]
+    # FIELD: list[bool] = [True, False]
 
-    print(
-        all((A or (A and B)) == A for A in FIELD for B in FIELD)
-    )
+    # print(
+    #     all((A or (A and B)) == A for A in FIELD for B in FIELD)
+    # )
 
 
 if __name__ == "__main__":
