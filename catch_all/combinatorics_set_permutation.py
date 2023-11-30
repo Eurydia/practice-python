7448fd4b72633b115bf73dfe6f5b7336e240f57a
Euryn <crossdeltavi@gmail.com>
2023-11-30 22:12:34 +0100

    Create combinatorics_set_permutation.py

---
+++ catch_all/combinatorics_set_permutation.py
@@ -0,0 +1,16 @@
+from typing import Set, List, Tuple
+from itertools import permutations
+
+
+def getPermutationsWithoutRep(A: Set[int]) -> List[Tuple[int, ...]]:
+
+
+
+def main() -> None:
+    A: Set[int] = {1, 2, 3, 4, 5}
+
+    print(f"Permutations: {getPermutations(A)}")
+
+
+if __name__ == "__main__":
+    main()
